e2fd2c141cf0e49b345f4c4b6dc8cc6224491dcd
youth <boysoeng@naver.com>
2025-01-17 22:46:02 +0900

    add swagger config

---
+++ src/main/java/com/cotato/team2/team2/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.cotato.team2.team2.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Team2 API 명세서", description = "YOUTH_DEV_SWAGGER 명세서", version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/**"};
+
+        return GroupedOpenApi.builder()
+                .group("스웨거 테스트 API v1")  // 그룹 이름을 설정한다.
+                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
+                .build();
+    }
+}
+
