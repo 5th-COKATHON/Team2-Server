0c17bcb67170f23c0cb0365b5ee5d17683b89c0b
youth <boysoeng@naver.com>
2025-01-17 21:36:23 +0900

    Chore: health check api

---
+++ src/main/java/com/cotato/team2/team2/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.cotato.team2.team2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("health");
+    }
+}
