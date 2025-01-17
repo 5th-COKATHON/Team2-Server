0e7dd2010325619c3836e6800367ab1930e36104
youth <boysoeng@naver.com>
2025-01-17 22:28:35 +0900

    Chore: remove test

--- src/test/java/com/cotato/team2/team2/Team2ApplicationTests.java
+++ src/test/java/com/cotato/team2/team2/Team2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Team2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
